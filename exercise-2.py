de632137ce4ac85c829061ff60e20501db081285
shaybay <pacificconnect310@gmail.com>
2022-04-19 15:59:21 -0700

    exercise3 done

--- exercise-2.py
+++ exercise-2.py
@@ -3,6 +3,20 @@
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase: 
+
+word = input('please enter a word or phrase: ').upper()
+if word != "quit":
+    while not word.isalpha():
+        print(f"{word} is not a word/phrase")
+        word = input('please enter a word or phrase: ').upper()
+    while word != "quit":
+        word.isalpha()
+        print(f'that word/phrase is {len(word)} charecters long')
+        word = input('please enter a word or phrase: ').upper()
+else:
+    print('goodbye')
+     
+
 # 2. Print the following message:
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
--- exercise-3.py
+++ exercise-3.py
@@ -3,9 +3,17 @@
 # Write the code that:
 # 1. Prompts the user to enter a dog's age in human years like this:
 #      Input a dog's age in human years: 
+dog = int(input('enter your dogs age: '))
 # 2. Calculates the equivalent dog years, where:
 #      - The first two years count as 10 years each
 #      - Any remaining years count as 7 years each
+if(dog>0): 
+    if dog<3:
+        h=dog*10
+        print(f"The dogs age in dog years is {h}")
+    elif dog>2:
+        h=(dog/dog)*20+(dog-2)*7
+        print(f"The dogs age in dog years is {h}")
 # 3. Prints the answer in the following format:
 #      The dog's age in dog years is xx
 
